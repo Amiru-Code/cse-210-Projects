1ced7d16293628f105310ce069f9c4b0c5a4bc73
Amiru-Code <jen25019@byui.edu>
2026-01-23 14:45:31 -0700

    Completed stretch 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        int num = 0;
+        List<int> numbers = new List<int>();
+        
+        
+        
+
+        do
+        {
+            Console.Write("Enter an integer: ");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out num))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+            numbers.Add(num);
+        }
+        while (num != 0);
+        
+        numbers.Remove(0);
+
+        int finalSum = 0;
+        int largeNum = 0;
+                
+        foreach(int n in numbers)
+        {
+            finalSum += n;
+            if(n>largeNum)
+                largeNum = n;
+            
+        }
+        
+        float floatFinalSum = (float)finalSum;
+
+        float avg = floatFinalSum / numbers.Count;
+
+
+
+
+        
+        Console.WriteLine($"The sum is {finalSum}, the average is {avg},  the largest number is {largeNum}");
+
+        
     }
 }
