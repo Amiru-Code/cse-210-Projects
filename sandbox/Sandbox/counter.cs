0305e848c55ef5e230bd94fe6de69ee724ab3bac
Amiru-Code <jen25019@byui.edu>
2026-01-29 14:10:07 -0700

    classStuff

---
+++ sandbox/Sandbox/counter.cs
@@ -0,0 +1,7 @@
+public class Counter
+{
+    public void Run()
+    {for(int i=0; i<10; i++)
+    Console.WriteLine(i);
+    }
+}
---
+++ sandbox/Sandbox/message.cs
@@ -0,0 +1,11 @@
+public class Message
+{
+    public string _message;
+
+    public int _priority;
+
+    public string GetMessage()
+    {
+        return $"{_priority}: {_message} ";
+    }
+}
